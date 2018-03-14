c35828a21ca0276d2aceb10a57a13828fe06eee6
Acejoy <36639911+Acejoy@users.noreply.github.com>
2018-03-14 22:07:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,4 +10,12 @@ function moveRight(){
 img.onclick= function () {
     var interval=setInterval(moveRight,50);
     
+};
+
+var count=document.getElementById('counter');
+button.onclick= function () {
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
 }
+
