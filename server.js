75fb20379af725139e61e24e0f2be1be1c339eae
Acejoy <36639911+Acejoy@users.noreply.github.com>
2018-03-15 11:14:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ var app = express();
 app.use(morgan('combined'));
 
 
+var names=[];
+app.get('/submit-name/:name' , function(req,res){
+   var name=req.query.name;
+   names.push(name);
+   res.send(JSON.stringify(names));
+});
+
+
 var articles={
     'article-one' :{
     title:'Article One|Srinjoy',
@@ -110,12 +118,7 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-var names=[];
-app.get('/submit-name/:name' , function(req,res){
-   var name=req.query.name;
-   names.push(name);
-   res.send(JSON.stringify(names));
-});
+
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
