651734f7d21fafc968007138afc30e5d85ea4c98
Acejoy <36639911+Acejoy@users.noreply.github.com>
2018-03-15 11:56:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -42,7 +42,6 @@
       var list='';
       for (var i=0;i<names.length;i++){
  -        list+='<li>'+names[i]+'</li>';
- +        list+='<li>'+names[i]+'</li>';
       }
       var ul=document.getElementById('nameList');
       ul.innerHTML=list;
