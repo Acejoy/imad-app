3d726a3f47ffc5a1312be7a38e7331f0c8498b7c
Acejoy <36639911+Acejoy@users.noreply.github.com>
2018-03-18 21:49:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
 var bodyParser=require('body-parser');
+var session=require('express-session');
 
 var config={
     user:'crazymukh',
@@ -15,7 +16,10 @@ var config={
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
-
+app.use(session({
+    secret:'somerandomSecretValue',
+    cookie:{maxAge:1000*60*60*24*30}
+}));
 
 var pool=new Pool(config);
 
@@ -173,6 +177,10 @@ app.post('/login',function(req,res){
                 var salt=dbString.split('$')[2];
                 var hashedPassword=hash(password,salt);
                 if (hashedPassword==dbString){
+                    
+                    req.session.auth={userId:result.rows[0].id};
+                    
+                    
                     res.send('credentials Correct');
                 }
                 else{
@@ -181,6 +189,19 @@ app.post('/login',function(req,res){
             }
         }
     }); 
+});
+
+
+
+app.get('/check-login',function(req,res){
+    
+    if (req.session &&req.session.auth && req.session.auth.userId){
+        res.send('You are logged in '+req.session.auth.userId.toString());
+    }else{
+        res.send('You are not logged in ');
+    }
+    
+    
 });
 
 app.get('/articles/:articleName',function(req,res){
