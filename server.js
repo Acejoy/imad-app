c8afc7fa313ca7829662736bca9e12945e44e753
Acejoy <36639911+Acejoy@users.noreply.github.com>
2018-03-18 17:49:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ var config={
 };
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyparser.json());
 
 
 var pool=new Pool(config);
@@ -135,6 +136,22 @@ function hash(input,salt){
 app.get('/hash/:input',function(req,res){
    var hashedString = hash(req.params.input,'this-is some-random-string');
    res.send(hashedString);
+});
+
+app.post('/create-user',function(req,res){
+    
+    var username=req.body.username;
+    var password=req.body.password;
+    
+    var salt =crypto.getrandomBytes(128).toString('hex');
+    var dbString=hash(password,salt);
+    pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)',[username,dbString],function(err,result){
+        if(err)
+            res.status(500).send(err.toString());
+        else
+            res.send('User successfully created:'+ username);
+    });
+    
 });
 
 app.get('/articles/:articleName',function(req,res){
