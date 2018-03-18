3e0af8593fc4c6102827cb6d25444fa0bea83d3f
Acejoy <36639911+Acejoy@users.noreply.github.com>
2018-03-18 20:09:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,50 +13,32 @@
      
  };
  
- 
- var button=document.getElementById('counter');
- button.onclick= function () {
-     var request=new  XMLHttpRequest();
-     request.onreadystatechange= function(){
-         if(request.readyState===XMLHttpRequest.DONE){
-             if(request.status===200){
-                 var counter=request.responseText;
-                 var span=document.getElementById('count');
-                 span.innerHTML=counter.toString();
-             }
-         }
-         
-     } ;
-     
-     request.open('GET','http://crazymukh.imad.hasura-app.io/counter',true);
-     request.send(null);
-     
- };
- 
- 
- 
+
  var submit=document.getElementById('submit_btn');
  submit.onclick= function () {
       var request=new  XMLHttpRequest();
      request.onreadystatechange= function(){
          if(request.readyState===XMLHttpRequest.DONE){
              if(request.status===200){
-                 var names=request.responseText;
-                 names=JSON.parse(names);
-                 var list='';
-                 for (var i=0; i<names.length; i++){
-                     list +='<li>' +names[i] + '</li>';
-                 }
-                 var ul=document.getElementById('nameList');
-                 ul.innerHTML=list;
+                console.log('user logged in');
+                alert('Logged in Successsfully');
+             }
+             else if (request.status === 403){
+                 alert('Username/password is incorrect');
+             }
+             else if (request.status === 500){
+                 alert('Soemthing went wrong');
              }
          }
          
      } ;
-     var nameInput=document.getElementById('name');
-     var name=nameInput.value;
+     var username=document.getElementById('username').value;
+     var password=document.getElementById('password').value;
+     console.log(username);
+     console.log(password);
      
-     request.open('GET','http://crazymukh.imad.hasura-app.io/submit-name?nu='+name,true);
-     request.send(null);
+     request.open('POST','http://crazymukh.imad.hasura-app.io/login',true);
+     request.setRequestHeader('Content-Type','application/json');
+     request.send(JSON.stringify({username:username,password:password}));
       
 };
